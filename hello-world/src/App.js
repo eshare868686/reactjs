4c33e2316bfecc0c83e1f11b099f22765c8ecf74
eshare868686 <122279932+eshare868686@users.noreply.github.com>
2023-05-14 19:26:13 +0700

    commit
    commit

---
+++ hello-world/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { Card } from './Compontents/Card';
+import { data } from './Data/data';
+
+function App() {
+  return (
+    <div className="App">
+      {data.map(x=>{
+        return(
+          <Card title={x.title}/>
+        )
+      })}
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-world/src/Compontents/Card.js
@@ -0,0 +1,11 @@
+export function Card ({title, image, decription}){
+    return (
+    <div className="Card">
+       <div className="Hinhanh">HinhANh</div>
+       <div className="Title">{title}</div>
+       <div className="noidung">Noidung</div>
+</div>
+)
+    
+    
+}
